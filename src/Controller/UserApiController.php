fcc8e761a748182a38b5f5ccd7c0896fc4c65c7a
Micah Webner <micah@hfcc.edu>
2023-10-04 19:39:32 -0400

    Add class constructor in UserApiController.

--- src/Controller/UserApiController.php
+++ src/Controller/UserApiController.php
@@ -13,22 +13,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserApiController extends AbstractController
 {
+    public function __construct(
+        private readonly ManagerRegistry $doctrine
+    ) {
+    }
+
     #[Route('/api/users', name: 'api_get_users')]
-    public function getUsers(Request $request, ManagerRegistry $doctrine): JsonResponse
+    public function getUsers(Request $request): JsonResponse
     {
         $key = $_ENV["JWT_SECRET"];
         $header = $request->headers->get('Authorization');
         $header = preg_replace('/^Bearer /', '', $header);
         try {
             $token = JWT::decode($header, new Key($key, 'HS256'));
-            $user = $doctrine->getRepository(User::class)->find($token->sub);
+            $user = $this->doctrine->getRepository(User::class)->find($token->sub);
         } catch (\Exception $e) {
             return $this->json([
                 'error' => $e->getMessage(),
             ]);
         }
         return $this->json([
-            'data' => $doctrine->getRepository(User::class)->findAll(),
+            'data' => $this->doctrine->getRepository(User::class)->findAll(),
         ]);
     }
 }
